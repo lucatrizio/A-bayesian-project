bd5fa99f29249e7be76e01131ccb361c7954efca
luca <luca.trizio@mail.polimi.it>
2023-12-24 19:05:10 +0100

    constructor parziale che genera pi, w, M, S con esempio

--- CAM.cpp
+++ CAM.cpp
@@ -1,1 +1,29 @@
-#include "prova.h"
+#include <armadillo>
+#include "CAM.h"
+
+using namespace arma;
+
+
+int main() {
+  arma::field<Mat> field_example(5, 3);  // Creerà un campo 3x3 di matrici vuote
+  for (arma::uword i = 0; i < field_example.n_elem; i++) {
+        field_example(i) = arma::randu(2, 2);  // Matrici 2x2 con valori casuali
+  }
+  int n_j = field_example.n_cols;
+  int J = 5; // numero persone
+  int K = 2; // numero di DC
+  int L = 3; // numero di OC
+  int V = 2; // dimensioni dati
+  //vec N = arma::ones<arma::vec>(J) * n_j;  // esempio, poi n_j magari non è uguale per tutti
+  int N = n_j;
+  Dimensions dim = Dimensions {J, K, L, V, N};
+  double forma_beta = 5.0;
+  double scala = 1.06;
+  // Generazione di un vettore di alpha
+  vec alpha = arma::randg<vec>(dim.K, arma::distr_param(forma_beta, scala));
+  vec beta = arma::randg<vec>(dim.L, arma::distr_param(forma_beta, scala));
+  Chain catena_example = Chain(dim, alpha, beta, field_example);
+  catena_example.print();
+
+  return 0;
+  }
--- CAM.h
+++ CAM.h
@@ -1,24 +1,85 @@
 #include <math.h>
 #include "vector"
-#include "armadillo.h"
+#include <armadillo>
+#include <random>
 
-struct dimensions{
+using namespace std;
+using namespace arma;
+
+struct Dimensions{
     int J; //n of persons
     int K; //n of DC
     int L; //n of OC
     int V; //dimensions of one data
-}; //non  credo nj serva
+    int N;
+}; //non  credo nj serva // si serve 
 
 
-class chain{
+// funzione per generare campioni da una dirichlet di con parametro alpha (vettore)
+vec generateDirichlet(const vec& alpha) {
+    int k = alpha.n_elem;
+    vec gammaSample(k);
+
+    // Genera k variabili casuali dalla distribuzione gamma
+    for (int i = 0; i < k; ++i) {
+        gammaSample(i) = randg(1, distr_param(alpha(i), 1.0))(0);
+    }
+
+    // Normalizza il campione per ottenere una variabile casuale Dirichlet
+    return gammaSample / sum(gammaSample);
+}
+
+
+
+class Chain{
+    /*
 private:
-    field<mat> data
-    dimensions dim;
-    vec pi;
-    vec alpha;
-    mat w;
-    mat beta;
-    Col<int> S;
-    Mat<int> M;
+    field<mat> data;
+    Dimensions dim;
+    vec pi; // (K x 1) pesi per scegliere DC
+    vec alpha; // (K x 1) K:= numero di DC
+    mat w; // (L x K) scelto K, pesi per scegliere OC
+    vec beta; // (L x 1) L := numero di OC
+    Col<int> S; // (J x 1) assegna un DC ad ogni persona (J persone)
+    Mat<int> M; // (J x max(n_j)) assegna per ogni persona un OC ad ogni atomo di quella persona (J persone e n_j atomo per persona j)
+    */
 public:
+    field<mat> data;
+    Dimensions dim;
+    vec pi; // (K x 1) pesi per scegliere DC
+    vec alpha; // (K x 1) K:= numero di DC
+    mat w; // (L x K) scelto K, pesi per scegliere OC
+    vec beta; // (L x 1) L := numero di OC
+    arma::Col<int> S; // (J x 1) assegna un DC ad ogni persona (J persone)
+    Mat<int> M; // (J x max(n_j)) assegna per ogni persona un OC ad ogni atomo di quella persona (J persone e n_j atomo per persona j)
+    Chain(const Dimensions& input_dim, const vec& input_alpha, const vec& input_beta, const arma::field<mat>& input_data) {
+        dim = input_dim;
+        alpha = input_alpha;
+        beta = input_beta;
+        data = input_data;
+        pi = generateDirichlet(alpha); // genera i pesi per DC dalla Dirichlet
+        for (int k = 0; k < dim.K; k++) {
+            vec w_k = generateDirichlet(beta);
+            w = join_rows(w, w_k); // genera i pesi per OC dalla Dirichlet
+        }
+        std::default_random_engine generatore_random;
+        discrete_distribution<int> Cat(pi.begin(), pi.end()); 
+        S.set_size(dim.J);
+        M.set_size(dim.J, dim.N);
+        for (int j = 0; j < dim.J; j++) {
+            S(j) = Cat(generatore_random); // genera S (vettore lungo J persone) con ogni elemento preso dalla categorica con pesi pi
+            discrete_distribution<int> Cat_w(w.col(S(j)).begin(), w.col(S(j)).end()); 
+            for (int i = 0; i < dim.N; i++) {
+                M(j,i) = Cat_w(generatore_random);
+            }
+        }
+        
+    }
+
+    void print(void) {
+        cout << "pi:\n" << pi << endl;
+        cout << "w:\n" << w << endl;
+        cout << "S:\n" << S << endl;
+        cout << "M:\n" << M << endl;
+    }
 };
