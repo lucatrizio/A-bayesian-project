3f0fc60a1a1fa8a8aa3e0f15dd5047500b3f5ba5
luca <luca.trizio@mail.polimi.it>
2023-12-25 10:05:47 +0100

    ordinato e commentato

--- CAM.cpp
+++ CAM.cpp
@@ -5,25 +5,37 @@ using namespace arma;
 
 
 int main() {
-  arma::field<Mat> field_example(5, 3);  // Creerà un campo 3x3 di matrici vuote
-  for (arma::uword i = 0; i < field_example.n_elem; i++) {
-        field_example(i) = arma::randu(2, 2);  // Matrici 2x2 con valori casuali
-  }
-  int n_j = field_example.n_cols;
-  int J = 5; // numero persone
-  int K = 2; // numero di DC
-  int L = 3; // numero di OC
-  int V = 2; // dimensioni dati
-  //vec N = arma::ones<arma::vec>(J) * n_j;  // esempio, poi n_j magari non è uguale per tutti
-  int N = n_j;
-  Dimensions dim = Dimensions {J, K, L, V, N};
-  double forma_beta = 5.0;
-  double scala = 1.06;
-  // Generazione di un vettore di alpha
-  vec alpha = arma::randg<vec>(dim.K, arma::distr_param(forma_beta, scala));
-  vec beta = arma::randg<vec>(dim.L, arma::distr_param(forma_beta, scala));
-  Chain catena_example = Chain(dim, alpha, beta, field_example);
-  catena_example.print();
 
-  return 0;
-  }
+      // Esempio di dati: 5 persone e 3 dati per persona, per ogni dato una matrice 2x2 (non vettore?)
+      arma::field<Mat> field_example(5, 3);  // Creerà un campo 3x3 di matrici vuote
+      for (arma::uword i = 0; i < field_example.n_elem; i++) {
+      field_example(i) = arma::randu(2, 2);  // Matrici 2x2 con valori casuali perche matrice e non vettore?
+      }
+
+      int n_j = field_example.n_cols; //numero di atomi, va cambiato quando non saranno tutti uguali (magari non saranno neanche tutti uguali)
+      int J = 5; // numero persone
+      int K = 2; // numero di DC
+      int L = 3; // numero di OC
+      int V = 2; // dimensione singola osservazione (vettore 2)
+      int N = n_j; // numero di osservazioni per persona
+      //vec N =    quando gli n_j non sono tutti uguali andrà passato un vettore (un n_j per ogni persona j)
+
+      // Dimensioni da passare alla catena
+      Dimensions dim = Dimensions {J, K, L, V, N};
+
+      // Parametri per generare le Dirichlet
+      double forma_beta = 5.0;
+      double scala = 1.06;
+
+      // Prior hyperparameters per le dirichlet
+      vec alpha = arma::randg<vec>(dim.K, arma::distr_param(forma_beta, scala));
+      vec beta = arma::randg<vec>(dim.L, arma::distr_param(forma_beta, scala));
+
+      // Esempio di catena
+      Chain catena_example = Chain(dim, alpha, beta, field_example);
+
+      // Stampiamo le prior create
+      catena_example.print();
+
+      return 0;
+}
--- CAM.h
+++ CAM.h
@@ -6,17 +6,34 @@
 using namespace std;
 using namespace arma;
 
+
+// TODO: Getter and Setter, spostare le variabili da public a private e cambiare di conseguenza il codice con i vari.get() e .set()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct Dimensions{
     int J; //n of persons
     int K; //n of DC
     int L; //n of OC
     int V; //dimensions of one data
-    int N;
+    int N; //numero di osservazioni per persona
 }; //non  credo nj serva // si serve 
 
 
 // funzione per generare campioni da una dirichlet di con parametro alpha (vettore)
 vec generateDirichlet(const vec& alpha) {
+
     int k = alpha.n_elem;
     vec gammaSample(k);
 
@@ -32,6 +49,8 @@ vec generateDirichlet(const vec& alpha) {
 
 
 class Chain{
+    // ANDREBBERO MESSE TUTTE LE VARIABILI NEL PRIOR E CREATI GETTERS E SETTERS
+
     /*
 private:
     field<mat> data;
@@ -43,43 +62,62 @@ private:
     Col<int> S; // (J x 1) assegna un DC ad ogni persona (J persone)
     Mat<int> M; // (J x max(n_j)) assegna per ogni persona un OC ad ogni atomo di quella persona (J persone e n_j atomo per persona j)
     */
+
+
 public:
-    field<mat> data;
-    Dimensions dim;
+    field<mat> data; //input
+    Dimensions dim; //input
     vec pi; // (K x 1) pesi per scegliere DC
-    vec alpha; // (K x 1) K:= numero di DC
+    vec alpha; // (K x 1) K:= numero di DC (passato in input)
     mat w; // (L x K) scelto K, pesi per scegliere OC
-    vec beta; // (L x 1) L := numero di OC
+    vec beta; // (L x 1) L := numero di OC (passato in input)
     arma::Col<int> S; // (J x 1) assegna un DC ad ogni persona (J persone)
     Mat<int> M; // (J x max(n_j)) assegna per ogni persona un OC ad ogni atomo di quella persona (J persone e n_j atomo per persona j)
+
+    // Costruttore 
+
+
     Chain(const Dimensions& input_dim, const vec& input_alpha, const vec& input_beta, const arma::field<mat>& input_data) {
+
         dim = input_dim;
         alpha = input_alpha;
         beta = input_beta;
         data = input_data;
+
+        // Generazione dei pesi pi e w
         pi = generateDirichlet(alpha); // genera i pesi per DC dalla Dirichlet
         for (int k = 0; k < dim.K; k++) {
-            vec w_k = generateDirichlet(beta);
-            w = join_rows(w, w_k); // genera i pesi per OC dalla Dirichlet
+            vec w_k = generateDirichlet(beta); //genera un vettore dalla dirichlet per ogni k
+            w = join_rows(w, w_k); // costruisce la matrice dei pesi aggiungendo ogni colonna
         }
+
+        // Generazione delle variabili categoriche S e M
         std::default_random_engine generatore_random;
+
         discrete_distribution<int> Cat(pi.begin(), pi.end()); 
+
         S.set_size(dim.J);
         M.set_size(dim.J, dim.N);
+        
         for (int j = 0; j < dim.J; j++) {
             S(j) = Cat(generatore_random); // genera S (vettore lungo J persone) con ogni elemento preso dalla categorica con pesi pi
-            discrete_distribution<int> Cat_w(w.col(S(j)).begin(), w.col(S(j)).end()); 
+            discrete_distribution<int> Cat_w(w.col(S(j)).begin(), w.col(S(j)).end()); //avendo deciso S per la persona j, costruisco la distribuzione categorica con i pesi associati a w_S(j)
             for (int i = 0; i < dim.N; i++) {
-                M(j,i) = Cat_w(generatore_random);
+                M(j,i) = Cat_w(generatore_random); // genera M(matrice J x N) per ogni persona (j), genera N valori da categoriche seguendo la distribuzione Cat_w
             }
         }
         
     }
 
+
+    // Stampa
     void print(void) {
         cout << "pi:\n" << pi << endl;
         cout << "w:\n" << w << endl;
         cout << "S:\n" << S << endl;
         cout << "M:\n" << M << endl;
     }
+
+
+
 };
